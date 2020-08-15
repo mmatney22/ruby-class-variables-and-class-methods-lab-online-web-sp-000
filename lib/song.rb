341f0317d4e57053c9fbe27a800b96f9411fe86a
Morgan Matney <morgan.matney89@gmail.com>
2020-08-15 18:18:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genre = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -13,4 +13,6 @@ class Song
   def self.count
     @@count
   end
+
+  def self.genres
 end
