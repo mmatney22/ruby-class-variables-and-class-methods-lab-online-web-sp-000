3bf03f65ee695c100d1f8391433144f5736aac07
Morgan Matney <morgan.matney89@gmail.com>
2020-08-15 18:22:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre.include?
+      if genre.include?(genre)
         genre_count[genre] += 1
       else
         genre_count[genre] = 1
